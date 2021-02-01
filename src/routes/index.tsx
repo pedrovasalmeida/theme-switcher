e92819acf79e6caf8edb32f5e89a7998e2898135
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-01 15:48:49 -0300

    removing not-found page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,10 @@ import { Switch, Route } from 'react-router-dom';
 
 /** Pages */
 import Home from '../pages/Home';
-import NotFound from '../pages/NotFound';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home} />
-
-    <Route path="/" component={NotFound} />
   </Switch>
 );
 

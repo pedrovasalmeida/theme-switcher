122acbb74a8e21b72ec60d1e53866cb98a0405a5
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-02-01 15:49:45 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 /** Components */
 import Routes from './routes';
+import { ThemeContextProvider } from './context/ThemeSwitcherContext';
 
 /** Global Styles */
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeContextProvider>
       <GlobalStyles />
 
       <Router>
         <Routes />
       </Router>
-    </>
+    </ThemeContextProvider>
   );
 };
 
